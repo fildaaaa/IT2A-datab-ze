cc5b4fa680b5622dc8d102c88220c195ac0d6bcd
fildaaaa <fildabrno3@gmail.com>
2026-04-01 11:56:09 +0200

    Add files via upload

---
+++ IT2A_databaze/IT2A_databaze/Auto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT2A_databaze
+{
+    public class Auto
+    {
+        public int ID { get; set; }
+        public string Znacka { get; set; }
+        public string Model { get; set; }
+        public int RokVyroby { get; set; }
+        public string Barva { get; set; }
+    }
+}
---
+++ IT2A_databaze/IT2A_databaze/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+
+namespace IT2A_databaze
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Auto> Auta { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Auta = new ObservableCollection<Auto>();
+            dgAuta.ItemsSource = Auta;
+        }
+
+        private void Pridat_Click(object sender, RoutedEventArgs e)
+        {
+            Auta.Add(new Auto
+            {
+                ID = Auta.Count + 1,
+                Znacka = "Škoda",
+                Model = "Superb",
+                RokVyroby = 2015,
+                Barva = "Červená"
+            });
+        }
+
+        private void Smazat_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgAuta.SelectedItem is Auto vybraneAuto)
+            {
+                Auta.Remove(vybraneAuto);
+            }
+        }
+    }
+}
